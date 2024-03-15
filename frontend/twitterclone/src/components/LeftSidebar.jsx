b5ce751cc162ce27f439dea4b549422cc26c80a4
Aditya Kale <kaditya21comp@student.mes.ac.in>
2024-03-15 18:01:29 +0530

    Lot of progress in Left Side Bar

--- frontend/twitterclone/src/components/LeftSidebar.jsx
+++ frontend/twitterclone/src/components/LeftSidebar.jsx
@@ -1,24 +1,51 @@
 import React from 'react'
-import LogoWhite from '../assets/logo_white.svg'
-import {HomeSelectedLight, HomeUnselectedLight} from './Media'
+import {LogoLight ,HomeLight, ExploreLight, NotificationsLight, MessagesLight, GrokLight, ListsLight, ProfileLight, MoreLight} from './Media'
 
-const SvgComponent = (props) => (
-  <svg aria-hidden="true" viewBox="0 0 24 24" {...props}>
-    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-  </svg>
-)
+const Element = ({icon, text, selected}) => {
+    if (selected === true) {
+    return (
+        <div className='flex w-max font-bold text-xl font-sans hover:bg-slate-200 rounded-full h-16 items-center'>
+            <div className='m-3'>{icon}</div>
+            <div className='m-3'>{text}</div>
+        </div>
+    )}
+    return (
+        <div className='flex w-max text-xl font-sans hover:bg-slate-200 rounded-full h-16 items-center'>
+            <div className='m-3'>{icon}</div>
+            <div className='m-3'>{text}</div>
+        </div>
+    )
+}
 
+const BlueButton = ({text}) => <button className='h-12 bg-sky-400 hover:bg-sky-500 rounded-full m-3 text-white text-base'> {text} </button>
+
+const UserButton = ({image, act_name, username}) => {
+    return (
+        <div className='flex'>
+            <div>{image}</div>
+            <div>
+                <div>{act_name}</div>
+                <div>@{username}</div>
+            </div>
+        </div>
+    )
+}
 
 const LeftSidebar = () => {
     return (
         <div>
-            <div>
-                <img src={LogoWhite} width={'28px'} />
-                <div>
-                    <SvgComponent width={'28px'} />
-                    <HomeSelectedLight width={'30px'} height={'30px'}/>
-                    <HomeUnselectedLight width={'30px'} height={'30px'}/>
-                </div>
+            <div className='inline-flex mt-1 mb-1 flex-col'>
+                <LogoLight className='m-3'/>
+                <Element icon={<HomeLight selected={true}/>} text={'Home'} selected={true}/>
+                <Element icon={<ExploreLight selected={false}/>} text={'Explore'} selected={false}/>
+                <Element icon={<NotificationsLight selected={false}/>} text={'Notifications'} selected={false}/>
+                <Element icon={<MessagesLight selected={false}/>} text={'Messages'} selected={false}/>
+                <Element icon={<GrokLight selected={false}/>} text={'Grok'} selected={false}/>
+                <Element icon={<ListsLight selected={false}/>} text={'Lists'} selected={false}/>
+                <Element icon={<LogoLight/>} text={'Premium'} selected={false}/>
+                <Element icon={<ProfileLight selected={false}/>} text={'Profile'} selected={false}/>
+                <Element icon={<MoreLight/>} text={'More'} selected={false}/>
+                <BlueButton text={'Post'}/>
             </div>
         </div>
     )
