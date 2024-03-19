e7fb5e6ac01b0969a133035450ae9f53575a0f8b
Aditya Kale <kaditya21comp@student.mes.ac.in>
2024-03-19 20:03:19 +0530

    Created Subscribe

--- frontend/twitterclone/src/components/RightSidebar.jsx
+++ frontend/twitterclone/src/components/RightSidebar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { SearchIcon } from './Media'
 
+const Subscribe = (props) => {
+    return (
+        <div className='bg-slate-200 h-40 w-[330px] max-lg:w-[290px] rounded-xl p-1 mt-3'>
+            <div className=' ml-5 h-max items-center justify-center pt-2'>
+                <h1 className='font-sans font-bold text-xl '>{props.title}</h1>
+                <p className='mt-2 mb-2'>Subscribe to unlock new features and if eligible, receive a share of ads revenue.</p>
+                <BlueButton text={'Subscribe'} />
+            </div>
+        </div >
+    )
+}
+
+const BlueButton = ({ text }) => <button className='h-9 bg-sky-500 hover:bg-sky-600 rounded-full mr-12 w-24 text-white text-base justify-center font-bold pl-2 pr-2'> {text} </button>;
+
 const SearchButton = () => {
     return (
-        <div className='flex bg-slate-200 mt-1 ml-8 items-center w-[330px] max-lg:w-[290px] rounded-full h-12'>
+        <div className='flex bg-slate-200 mt-1 items-center w-[330px] max-lg:w-[290px] rounded-full h-12'>
             <SearchIcon className='ml-5' />
             <input className='focus:outline-none pl-4 ml-1 h-11 bg-slate-200' placeholder='Search'></input>
         </div>
@@ -13,7 +27,12 @@ const SearchButton = () => {
 const RightSidebar = () => {
     return (
         <div className='w-[28%] border-l border-slate-200'>
-            <SearchButton />
+            <div className='ml-8'>
+                <SearchButton />
+                <Subscribe title={"Subscribe to Premium"} content={"hello"} />
+
+            </div>
+
         </div>
     )
 }
