3d72e306f719cbe3ad6b9706eab1e0b67d48dd3e
Aditya Kale <kaditya21comp@student.mes.ac.in>
2024-03-21 19:39:30 +0530

    Completing right sidebar and home component and making things more responsive

--- frontend/twitterclone/src/components/Feed.jsx
+++ frontend/twitterclone/src/components/Feed.jsx
@@ -4,104 +4,104 @@ import Avatar from 'react-avatar';
 import TextareaAutosize from 'react-textarea-autosize';
 
 const TopButtons = () => {
-  return (
-    <div className='flex justify-between border-b border-slate-200'>
-      <button className='w-64 h-14 hover:bg-slate-200 transition-all font-sans'>For You</button>
-      <button className='w-72 h-14 hover:bg-slate-200 transition-all font-sans'>Following</button>
-      <div className='w-14 h-14 align-middle items-center justify-center flex'>
-        <div className='w-9 h-9 hover:bg-slate-200 rounded-full justify-center items-center flex transition-all'>
-          <SettingsLight />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex justify-between border-b border-slate-200'>
+			<button className='w-64 h-14 hover:bg-slate-200 transition-all font-sans'>For You</button>
+			<button className='w-72 h-14 hover:bg-slate-200 transition-all font-sans'>Following</button>
+			<div className='w-14 h-14 align-middle items-center justify-center flex'>
+				<div className='w-9 h-9 hover:bg-slate-200 rounded-full justify-center items-center flex transition-all'>
+					<SettingsLight />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 const InputTweet = ({ image }) => {
-  return (
-    <div>
-      <div className='flex m-5'>
-        <Avatar name={image} className='rounded-full mr-3' size='3rem' />
-        <form className='flex flex-col flex-grow pt-1 pl-3'>
-          <TextareaAutosize className='w-full focus:outline-none resize-none text-2xl mb-5' placeholder="What is happening?!" />
-          <div className='flex justify-between'>
-            <div className='flex items-center'>
-              <div className='h-9 w-9 hover:bg-blue-100 justify-center items-center rounded-full flex'>
-                <ImageIcon className='fill-sky-500' />
-              </div>
-              <div className='h-9 w-9 hover:bg-blue-100 justify-center items-center rounded-full flex'>
-                <GIFIcon className='fill-sky-500' />
-              </div>
-              <div className='h-9 w-9 hover:bg-blue-100 justify-center items-center rounded-full flex'>
-                <PollIcon className='fill-sky-500' />
-              </div>
-              <div className='h-9 w-9 hover:bg-blue-100 justify-center items-center rounded-full flex'>
-                <EmojiIcon className='fill-sky-500' />
-              </div>
-              <div className='h-9 w-9 hover:bg-blue-100 justify-center items-center rounded-full flex'>
-                <ScheduleIcon className='fill-sky-500' />
-              </div>
-            </div>
-            <BlueButton text={"Post"} />
-          </div>
-        </form>
-      </div>
-      <div className='border-b border-slate-200 mt-7 mb-7'></div>
-    </div>
-  );
+	return (
+		<div>
+			<div className='flex m-5'>
+				<Avatar name={image} className='rounded-full mr-3' size='3rem' />
+				<form className='flex flex-col flex-grow pt-1 pl-3'>
+					<TextareaAutosize className='w-full focus:outline-none resize-none text-2xl mb-5' placeholder="What is happening?!" />
+					<div className='flex justify-between'>
+						<div className='flex items-center'>
+							<div className='h-9 w-9 hover:bg-blue-100 justify-center items-center rounded-full flex'>
+								<ImageIcon className='fill-sky-500' />
+							</div>
+							<div className='h-9 w-9 hover:bg-blue-100 justify-center items-center rounded-full flex'>
+								<GIFIcon className='fill-sky-500' />
+							</div>
+							<div className='h-9 w-9 hover:bg-blue-100 justify-center items-center rounded-full flex'>
+								<PollIcon className='fill-sky-500' />
+							</div>
+							<div className='h-9 w-9 hover:bg-blue-100 justify-center items-center rounded-full flex'>
+								<EmojiIcon className='fill-sky-500' />
+							</div>
+							<div className='h-9 w-9 hover:bg-blue-100 justify-center items-center rounded-full flex'>
+								<ScheduleIcon className='fill-sky-500' />
+							</div>
+						</div>
+						<BlueButton text={"Post"} />
+					</div>
+				</form>
+			</div>
+			<div className='border-b border-slate-200 mt-7 mb-7'></div>
+		</div>
+	);
 };
 
 
 const TweetElements = ({ icon, count }) => {
-  return (
-    <div className='flex items-center justify-center rounded-full hover:bg-slate-200 mr-14 lg:mr-28'>
-      {icon} <span className='text-xs sm:text-sm'>{count}</span>
-    </div>
-  );
+	return (
+		<div className='flex items-center justify-center rounded-full hover:bg-slate-200 mr-14 lg:mr-28'>
+			{icon} <span className='text-xs sm:text-sm'>{count}</span>
+		</div>
+	);
 };
 
 
 const Tweets = ({ image, act_name, username, time }) => {
-  return (
-    <div>
-      <div className='flex flex-col m-5 mr-9'>
-        <div className='flex items-center'>
-          <Avatar name={image} className='rounded-full mr-3' size='3rem' />
-          <div className='font-sans flex flex-col'>
-            <span className='font-bold'>{act_name} </span><span className='text-nowrap font-normal text-slate-500'> @{username} · {time}</span>
-          </div>
-        </div>
-        <div className='ml-16 2xl:ml-9'>
-          "When returning information in a structured format the fields can be a myriad of types: string, boolean, integers. One of the hardest types to correctly handle is high-cardinality categorical values (or enums)"
-          <div className='flex mt-3'>
-            <TweetElements icon={<MessageIcon className='fill-slate-500' />} count={5} />
-            <TweetElements icon={<RepostIcon className='fill-slate-500' />} count={11} />
-            <TweetElements icon={<LikeIcon className='fill-slate-500' />} count={7} />
-            <TweetElements icon={<ViewIcon className='fill-slate-500' />} count={15} />
-          </div>
-        </div>
-      </div>
-      <div className='border-b border-slate-200 mt-5'></div>
-    </div>
-  );
+	return (
+		<div>
+			<div className='flex flex-col m-5 mr-9'>
+				<div className='flex items-center'>
+					<Avatar name={image} className='rounded-full mr-3' size='3rem' />
+					<div className='font-sans flex flex-col'>
+						<span className='font-bold'>{act_name} </span><span className='text-nowrap font-normal text-slate-500'> @{username} · {time}</span>
+					</div>
+				</div>
+				<div className='ml-9 2xl:ml-16'>
+					"When returning information in a structured format the fields can be a myriad of types: string, boolean, integers. One of the hardest types to correctly handle is high-cardinality categorical values (or enums)"
+					<div className='flex mt-3'>
+						<TweetElements icon={<MessageIcon className='fill-slate-500' />} count={5} />
+						<TweetElements icon={<RepostIcon className='fill-slate-500' />} count={11} />
+						<TweetElements icon={<LikeIcon className='fill-slate-500' />} count={7} />
+						<TweetElements icon={<ViewIcon className='fill-slate-500' />} count={15} />
+					</div>
+				</div>
+			</div>
+			<div className='border-b border-slate-200 mt-5'></div>
+		</div>
+	);
 };
 
 const BlueButton = ({ text }) => <button className='h-9 bg-sky-500 hover:bg-sky-600 rounded-full mr-2 w-20 text-white text-base font-bold'> {text} </button>;
 
 const Feed = () => {
-  return (
-    <div className='w-[100%] flex flex-col flex-grow overflow-y-auto h-screen'>
-      <TopButtons />
-      <div className="flex-grow">
-        <InputTweet image={'AK'} />
-        <Tweets image={'AK'} act_name="Aditya Kale" username='adityadkale' time={"2h"} />
-        <Tweets image={'AKH'} act_name="Aditya Khade" username='adityakhadeak' time={"2h"} />
-        <Tweets image={'RJAIN'} act_name="Rushabh Jain" username='rjain' time={"2h"} />
-        <Tweets image={'MM'} act_name="Mayur Mane" username='mayur' time={"2h"} />
-        <Tweets image={'AK'} act_name="Aditya Kale" username='adityadkale' time={"2h"} />
-      </div>
-    </div>
-  );
+	return (
+		<div className='w-[50%] border-r border-slate-200 flex flex-col max-2xl:w-[100%]'>
+			<TopButtons />
+			<div className="flex-grow">
+				<InputTweet image={'AK'} />
+				<Tweets image={'AK'} act_name="Aditya Kale" username='adityadkale' time={"2h"} />
+				<Tweets image={'AKH'} act_name="Aditya Khade" username='adityakhadeak' time={"2h"} />
+				<Tweets image={'RJAIN'} act_name="Rushabh Jain" username='rjain' time={"2h"} />
+				<Tweets image={'MM'} act_name="Mayur Mane" username='mayur' time={"2h"} />
+				<Tweets image={'AK'} act_name="Aditya Kale" username='adityadkale' time={"2h"} />
+			</div>
+		</div>
+	);
 };
 
 export default Feed;
--- frontend/twitterclone/src/components/RightSidebar.jsx
+++ frontend/twitterclone/src/components/RightSidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { SearchIcon } from './Media'
+import { SearchIcon, BreadCrumb } from './Media'
+import Avatar from 'react-avatar'
 
 const Subscribe = (props) => {
     return (
-        <div className='bg-slate-200 h-40 w-[330px] max-lg:w-[290px] rounded-xl p-1 mt-3'>
+        <div className='bg-slate-100 h-40 w-[330px] max-lg:w-[290px] rounded-xl p-1 mt-3'>
             <div className=' ml-5 h-max items-center justify-center pt-2'>
                 <h1 className='font-sans font-bold text-xl '>{props.title}</h1>
                 <p className='mt-2 mb-2'>Subscribe to unlock new features and if eligible, receive a share of ads revenue.</p>
@@ -13,24 +14,91 @@ const Subscribe = (props) => {
     )
 }
 
+const TrendingElement = ({ header, tag, no_of_posts }) => {
+    return (
+        <div className='mt-6 mb-2 mr-3 flex place-items-center'>
+            <div>
+                <p className='font-sans text-slate-500 text-xs'>{header}</p>
+                <p className='font-sans font-bold'>#{tag}</p>
+                <p className='font-sans text-slate-500 text-xs'>{no_of_posts} posts</p>
+            </div>
+            <div className='ml-auto'>
+                <BreadCrumb className='fill-slate-500' />
+            </div>
+        </div>
+    )
+}
+
+const TrendingBox = () => {
+    return (
+        <div className='bg-slate-100 w-[330px] max-lg:w-[290px] rounded-xl p-1 mt-3'>
+            <div className=' ml-5 h-max items-center justify-center pt-2 mb-6'>
+                <h1 className='font-sans font-bold text-xl '>What's happening</h1>
+                <TrendingElement header={"Trending in India"} tag={"stockmarkets"} no_of_posts={"15.3K"} />
+                <TrendingElement header={"Trending in India"} tag={"Angular"} no_of_posts={"15.3K"} />
+                <TrendingElement header={"Trending in India"} tag={"Binod"} no_of_posts={"15.3K"} />
+                <TrendingElement header={"Trending in India"} tag={"YalgaarHo"} no_of_posts={"15.3K"} />
+                <div className='font-sans text-sm mt-6 text-sky-600'>Show More</div>
+            </div>
+        </div>
+    )
+}
+
+
+const UserButton = ({ image, act_name, username }) => {
+    return (
+        <div className='flex place-items-center hover:bg-slate-200 rounded-full mt-5 mb-5'>
+            <div>
+                <Avatar name={image} className='rounded-full mr-3' size='3rem' />
+            </div>
+            <div className='max-lg:hidden'>
+                <div className='font-sans font-bold'>{act_name}</div>
+                <div className='text-slate-500'>@{username}</div>
+            </div>
+            <div className='align-middle ml-auto max-lg:hidden'>
+                <BlackButton text={"Follow"} />
+            </div>
+        </div>
+    );
+};
+
+
+const FollowBox = () => {
+    return (
+        <div className='bg-slate-100 w-[330px] max-lg:w-[290px] rounded-xl p-1 mt-3'>
+            <div className=' ml-5 h-max items-center justify-center pt-2 mb-6'>
+                <h1 className='font-sans font-bold text-xl mb-6'>Who to follow</h1>
+                <UserButton image={"Akh"} act_name={"Aditya Khade"} username={'adityakhadeak'} />
+                <UserButton image={'RJAIN'} act_name="Rushabh Jain" username='rjain' time={"2h"} />
+                <UserButton image={'MM'} act_name="Mayur Mane" username='mayur' time={"2h"} />
+                <div className='font-sans text-sm mt-6 text-sky-600'>Show More</div>
+            </div>
+        </div>
+    )
+}
+
+
+const BlackButton = ({ text }) => <button className='h-9 bg-black hover:bg-sky-600 rounded-full mr-2 w-20 text-white text-base font-bold'> {text} </button>;
+
 const BlueButton = ({ text }) => <button className='h-9 bg-sky-500 hover:bg-sky-600 rounded-full mr-12 w-24 text-white text-base justify-center font-bold pl-2 pr-2'> {text} </button>;
 
 const SearchButton = () => {
     return (
-        <div className='flex bg-slate-200 mt-1 items-center w-[330px] max-lg:w-[290px] rounded-full h-12'>
+        <div className='flex bg-slate-100 mt-1 items-center w-[330px] max-lg:w-[290px] rounded-full h-12'>
             <SearchIcon className='ml-5' />
-            <input className='focus:outline-none pl-4 ml-1 h-11 bg-slate-200' placeholder='Search'></input>
+            <input className='focus:outline-none pl-4 ml-1 h-11 bg-slate-100' placeholder='Search'></input>
         </div>
     )
 }
 
 const RightSidebar = () => {
     return (
-        <div className='w-[28%] border-l border-slate-200 max-2xl:hidden'>
+        <div className='sticky top-0 w-[28%] max-2xl:hidden'>
             <div className='ml-8'>
                 <SearchButton />
                 <Subscribe title={"Subscribe to Premium"} content={"hello"} />
-
+                <TrendingBox />
+                <FollowBox />
             </div>
 
         </div>
